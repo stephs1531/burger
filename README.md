cdfd2e3b2579f70f7815ef2e478ddb3cd1292c27
Stephanie Sapp <stephs1531@gmail.com>
2019-06-17 19:11:15 -0500

    connection to index.handlebars working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # burger
 Eat-Da-Burger Restaurant App
+
+** Use heroku postgres sql if jaws isn't working **
+
+
+taking note of the correct order from week 15 activities:
+start with config files
+get connections to apis and other files working
+build front end
+get onclick events working in the front end once connections are working
--- server.js
+++ server.js
@@ -21,8 +21,23 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
+var connection = require("./config/connection.js");
 
 app.use(routes);
+
+//=====================================================
+//ROUTES
+
+app.get("/", function(req, res) {
+  //we're doing the get when we load the page
+  connection.query("SELECT * FROM burgers;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { burgers: data });
+  });
+});
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
