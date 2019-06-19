d2bc9b8e0fbc76b8a028cf80ae538ddb8fb741c4
Stephanie Sapp <stephs1531@gmail.com>
2019-06-19 08:37:52 -0500

    added JawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Ginger998!",
-  database: "burgers_db"
-});
+
+
+//JawsDB connection
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Ginger998!",
+    database: "burgers_db"
+  });
+
+};
 
 // Make connection.
 connection.connect(function(err) {
